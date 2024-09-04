841c5b2e5d77fe7bf7c976461c2df1de167ef7cc
Kalyani <kalyanisbabu2000@gmail.com>
2024-09-04 19:19:29 +0100

    self attempt

--- Loops/main.py
+++ Loops/main.py
@@ -9,3 +9,28 @@ if temp<0:
 
 
 # 'it is freezing: TRUE'
+
+### EXERCISE 1 ###
+"""
+You have a budget of £10 and want to write a program to decide which burger restaurant to go to.
+
+Input the price of a burger using input()
+Check whether the price is less than or equal (<=) 10.00
+Print the result in the format below
+Burger is within budget: True
+
+Hint: remember to convert the input from a string to a decimal with float()
+
+"""
+#Input the price of a burger using input()
+price = float(input("Please input the price of the burger (no currency, please)"))
+condition = price<=10.00
+#Check whether the price is less than or equal (<=) 10.00
+if condition:
+    print(f"Burger is within budget: {condition}")
+
+#Print the result in the format below
+#Burger is within budget: True
+
+
+
