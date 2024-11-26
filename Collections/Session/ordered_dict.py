8e76f9eb4608a37ff870842c1feb50eff26fcbd0
Kalyani <kalyanisbabu2000@gmail.com>
2024-11-26 11:01:10 +0000

    practice OrderedDict

--- Collections/Session/ordered_dict.py
+++ Collections/Session/ordered_dict.py
@@ -11,3 +11,11 @@ o = OrderedDict()  # we can initialise an empty ordered dictionary
 o['bowls'] = 7     # and then populate it
 o['plates'] = 20
 print(o)
+print(o.items())
+
+some = OrderedDict([('plates', 6), ('spoons', 18), ('forks', 2)])
+print(some)
+print(some.items())
+print(some.keys())
+print(some.values())
+print(OrderedDict(reversed(some.items())))
