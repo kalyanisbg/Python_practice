e927e48fbf55edc992b4e51678506452869e008e
Kalyani <kalyanisbabu2000@gmail.com>
2024-11-26 11:41:12 +0000

    practice defaultdict

--- Collections/Session/default_dict.py
+++ Collections/Session/default_dict.py
@@ -3,4 +3,9 @@ from collections import defaultdict
 state_capitals = defaultdict(str)
 print(state_capitals)
 
+state_capitals['Alaska'] = 'Missouri'
 print(state_capitals['Alaska'])
+state_capitals['Kerala'] = 'Thiruvananthapuram'
+state_capitals['TamilNadu'] = 'Chennai'
+print(state_capitals['Pondicherry'])
+print("End of execution")
